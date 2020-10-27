ddf243f3b6cfd03c25734f20e5688dc8fbf76ec8
Ksawery <ksawery@superherocheesecake.com>
2020-10-27 11:56:55 +0100

    initial badge generator. v2

--- js/badge-generator.js
+++ js/badge-generator.js
@@ -3,7 +3,11 @@ window.onload = function () {
 };
 
 function generateBadge() {
+    let name = 'name';
 
+    let adjective1 = 'Bitter';
+    let adjective2 = 'Idiot';
+    let adjective3 = 'Moron';
     const svg = `
     <svg xmlns="http://www.w3.org/2000/svg" width="315" height="315" viewBox="0 0 315 315">
         <g id="Rectangle_287" data-name="Rectangle 287" transform="translate(12 11)" stroke="#000" stroke-width="5">
@@ -15,18 +19,16 @@ function generateBadge() {
             <rect x="2.5" y="2.5" width="298" height="299" rx="17.5" fill="none"/>
         </g>
         <g id="Group_238" data-name="Group 238" transform="translate(-7014 -13595)">
-            <text id="_1.some_adjective" data-name="1.some adjective" transform="translate(7166 13733)" font-size="31" font-family="Monarcha-Bold, Monarcha" font-weight="700"><tspan x="-128.898" y="0">1.some adjective</tspan></text>
-            <text id="_2.some_adjective" data-name="2.some adjective" transform="translate(7166 13783)" font-size="31" font-family="Monarcha-Bold, Monarcha" font-weight="700"><tspan x="-131.27" y="0">2.some adjective</tspan></text>
-            <text id="_3.some_adjective" data-name="3.some adjective" transform="translate(7166 13833)" font-size="31" font-family="Monarcha-Bold, Monarcha" font-weight="700"><tspan x="-130.975" y="0">3.some adjective</tspan></text>
-            <text id="Tom_is_a" data-name="Tom is a" transform="translate(7158 13683)" font-size="31" font-family="Monarcha-BoldItalic, Monarcha" font-weight="700" font-style="italic"><tspan x="-60.093" y="0">Tom is a</tspan></text>
+            <text id="_1.some_adjective" data-name="1.some adjective" transform="translate(7166 13733)" font-size="31" font-family="Monarcha-Bold, Monarcha" font-weight="700"><tspan x="-128.898" y="0">${adjective1}</tspan></text>
+            <text id="_2.some_adjective" data-name="2.some adjective" transform="translate(7166 13783)" font-size="31" font-family="Monarcha-Bold, Monarcha" font-weight="700"><tspan x="-131.27" y="0">${adjective2}</tspan></text>
+            <text id="_3.some_adjective" data-name="3.some adjective" transform="translate(7166 13833)" font-size="31" font-family="Monarcha-Bold, Monarcha" font-weight="700"><tspan x="-130.975" y="0">${adjective3}</tspan></text>
+            <text id="Tom_is_a" data-name="Tom is a" transform="translate(7158 13683)" font-size="31" font-family="Monarcha-BoldItalic, Monarcha" font-weight="700" font-style="italic"><tspan x="-60.093" y="0">${name} is a</tspan></text>
         </g>
     </svg>
 `
-
-
     svgToPng(svg, (imgData) => {
         const pngImage = document.createElement('img');
-        document.querySelector('.page__subject').appendChild(pngImage);
+        //document.querySelector('.page__subject').appendChild(pngImage);
         // document.body.appendChild(pngImage);
         pngImage.src = imgData;
 
@@ -47,13 +49,16 @@ function generateBadge() {
         const svgImage = document.createElement('img');
         // imgPreview.style.position = 'absolute';
         // imgPreview.style.top = '-9999px';
-        document.body.appendChild(svgImage);
+
+        document.querySelector('.page__subject').appendChild(svgImage);
+        //document.body.appendChild(svgImage);
         svgImage.onload = function () {
             const canvas = document.createElement('canvas');
             canvas.width = svgImage.clientWidth;
             canvas.height = svgImage.clientHeight;
             const canvasCtx = canvas.getContext('2d');
             canvasCtx.drawImage(svgImage, 0, 0);
+            canvasCtx.fillStyle = 'blue';
             const imgData = canvas.toDataURL('image/png');
             callback(imgData);
             // document.body.removeChild(imgPreview);
